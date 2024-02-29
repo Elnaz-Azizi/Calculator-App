c8f8f0bf544e54ab2f2b8de77cd8fe701f88faa6
Elnaz <azizi.elnaz66@gmail.com>
2024-02-29 13:02:01 +0100

    Calculator App

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -6,41 +6,27 @@ import java.util.Scanner;
  * Hello world!
  */
 public class App {
+    private static int operation; // Declare operation outside main method
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean isContinue = true;
+
         do {
             displayMenu();
             System.out.println("Choose an operation:");
             int operation = scanner.nextInt();
-
             if (operation == 1) {
-                System.out.println("Enter a number:");
-                int n1 = scanner.nextInt();
-                System.out.println("Enter a number:");
-                int n2 = scanner.nextInt();
-                int result = n1 + n2;
-                System.out.println("result = (" + n1 + " + " + n2 + "): " + result);
+                int result = addition();
+                System.out.println("result = " + result);
             } else if (operation == 2) {
-                System.out.println("Enter a number:");
-                int n1 = scanner.nextInt();
-                System.out.println("Enter a number:");
-                int n2 = scanner.nextInt();
-                int result = n1 - n2;
+                int result = subtraction();
                 System.out.println("result: =" + result);
             } else if (operation == 3) {
-                System.out.println("Enter a number");
-                int n1 = scanner.nextInt();
-                System.out.println("Enter a number");
-                int n2 = scanner.nextInt();
-                int result = n1 * n2;
+                int result = multiplication();
                 System.out.println("result: =" + result);
             } else if (operation == 4) {
-                System.out.println("Enter a number");
-                int n1 = scanner.nextInt();
-                System.out.println("Enter a number");
-                int n2 = scanner.nextInt();
-                int result = n1 / n2;
+                int result = division();
                 System.out.println("result: =" + result);
             } else if (operation == 5) {
                 System.out.println("Application closed");
@@ -49,6 +35,15 @@ public class App {
                 System.out.println("Invalid Operation Number");
 
             }
+            if (isContinue) {
+                System.out.println("Enter 1 to perform another calculation or any other number to exit:");
+                operation = scanner.nextInt();
+                if (operation != 1) {
+                    System.out.println("Calculator closed.");
+                    scanner.close();
+                }
+            }
+
         } while (isContinue);
 
 
@@ -63,5 +58,45 @@ public class App {
         System.out.println("5. Exit ");
     }
 
+    public static int addition() {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a number:");
+        int n1 = scanner.nextInt();
+        System.out.println("Enter a number:");
+        int n2 = scanner.nextInt();
+        int result = n1 + n2;
+        //System.out.println("result = (" + n1 + " + " + n2 + "): " + result);
+        return result;
+    }
+
+    public static int subtraction() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n1 = scanner.nextInt();
+        System.out.println("Enter a number:");
+        int n2 = scanner.nextInt();
+        int result = n1 - n2;
+        return result;
+    }
+
+    public static int multiplication() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n1 = scanner.nextInt();
+        System.out.println("Enter a number");
+        int n2 = scanner.nextInt();
+        int result = n1 * n2;
+        return result;
+    }
+
+    public static int division() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n1 = scanner.nextInt();
+        System.out.println("Enter a number");
+        int n2 = scanner.nextInt();
+        int result = n1 / n2;
+        return result;
+    }
 }
